12862c468f9269a8298c76b1df931b1e6eaedb46
Veelow92 <v.nkz92@gmail.com>
2024-07-01 01:32:09 +0800

    First day

---
+++ bitboardchess.c
@@ -0,0 +1,31 @@
+
+
+// system headers
+#include <stdio.h>
+
+// define bitboard data type
+#define U64 unsigned long long
+
+// bit manipulations
+
+// print bitboard
+void print_bitboard(U64 bitboard) {
+
+
+
+    
+}
+
+
+
+
+
+
+int main() {
+
+    printf("Hello");
+
+
+
+
+}
---
+++ makefile
@@ -0,0 +1,3 @@
+all:
+	gcc -oFast bitboardchess.c -o bitboardchess
+	x86_64-w64-mingw32-gcc -oFast bitboardchess.c -o bitboardchess
