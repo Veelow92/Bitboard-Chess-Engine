6aeb71808db90920e9f020eea39be4dc059f70f2
Veelow92 <v.nkz92@gmail.com>
2024-08-13 22:30:21 +0800

    Added UCI function.

--- bitboardchess.c
+++ bitboardchess.c
@@ -7,6 +7,7 @@
 \*************************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #ifdef WIN64
     #include <windows.h>
@@ -413,17 +414,17 @@ void print_board() {
         
     }
 
-    // print board files
-    printf("\n     a b c d e f g h\n\n");
+     // print board files
+    printf("\n      a b c d e f g h\n\n");
     
     // print side to move
-    printf("     Side:     %s\n", !side ? "white" : "black");
+    printf("      Side:     %s\n", !side ? "white" : "black");
     
     // print enpassant square
-    printf("     Enpassant:   %s\n", (enpassant != no_sq) ? square_to_coordinates[enpassant] : "no");
+    printf("      Enpassant:   %s\n", (enpassant != no_sq) ? square_to_coordinates[enpassant] : "no");
     
     // print castling rights
-    printf("     Castling:  %c%c%c%c\n\n", (castle & wk) ? 'K' : '-',
+    printf("      Castling:  %c%c%c%c\n\n", (castle & wk) ? 'K' : '-',
                                            (castle & wq) ? 'Q' : '-',
                                            (castle & bk) ? 'k' : '-',
                                            (castle & bq) ? 'q' : '-');
@@ -1565,11 +1566,18 @@ static inline void add_move(moves *move_list, int move)
 // print move (for UCI purposes)
 void print_move(int move)
 {
-    printf("%s%s%c\n", square_to_coordinates[get_move_source(move)],
+    if (get_move_promoted(move))
+    {
+        printf("%s%s%c\n", square_to_coordinates[get_move_source(move)],
                      square_to_coordinates[get_move_target(move)],
                      promoted_pieces[get_move_promoted(move)]);
+    }
+    else
+    {
+        printf("%s%s\n", square_to_coordinates[get_move_source(move)],
+                     square_to_coordinates[get_move_target(move)]);
+    }
 }
-
 
 // print move list
 void print_move_list(moves *move_list)
@@ -2435,6 +2443,284 @@ void perft_test(int depth)
 
 /*************************************************\
 ===================================================
+                Search position
+===================================================
+\*************************************************/
+
+// search position
+void search_position(int depth)
+{
+    // best move placeholder
+    printf("bestmove d2d4\n");
+}
+
+/*************************************************\
+===================================================
+                UCI
+===================================================
+\*************************************************/
+
+// parse user/GUI move string input (e.g. "e7e8q")
+int parse_move(char *move_string)
+{
+    // create move list instance
+    moves move_list[1];
+    
+    // generate moves
+    generate_moves(move_list);
+    
+    // parse source square
+    int source_square = (move_string[0] - 'a') + (8 - (move_string[1] - '0')) * 8;
+    
+    // parse target square
+    int target_square = (move_string[2] - 'a') + (8 - (move_string[3] - '0')) * 8;
+    
+    // loop over the moves within a move list
+    for (int move_count = 0; move_count < move_list->count; move_count++)
+    {
+        // init move
+        int move = move_list->moves[move_count];
+        
+        // make sure source & target squares are available within the generated move
+        if (source_square == get_move_source(move) && target_square == get_move_target(move))
+        {
+            // init promoted piece
+            int promoted_piece = get_move_promoted(move);
+            
+            // promoted piece is available
+            if (promoted_piece)
+            {
+                // promoted to queen
+                if ((promoted_piece == Q || promoted_piece == q) && move_string[4] == 'q')
+                    // return legal move
+                    return move;
+                
+                // promoted to rook
+                else if ((promoted_piece == R || promoted_piece == r) && move_string[4] == 'r')
+                    // return legal move
+                    return move;
+                
+                // promoted to bishop
+                else if ((promoted_piece == B || promoted_piece == b) && move_string[4] == 'b')
+                    // return legal move
+                    return move;
+                
+                // promoted to knight
+                else if ((promoted_piece == N || promoted_piece == n) && move_string[4] == 'n')
+                    // return legal move
+                    return move;
+                
+                // continue the loop on possible wrong promotions (e.g. "e7e8f")
+                continue;
+            }
+            
+            // return legal move
+            return move;
+        }
+    }
+    
+    // return illegal move
+    return 0;
+}
+
+/*
+    Example UCI commands to init position on chess board
+    
+    // init start position
+    position startpos
+    
+    // init start position and make the moves on chess board
+    position startpos moves e2e4 e7e5
+    
+    // init position from FEN string
+    position fen r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1 
+    
+    // init position from fen string and make moves on chess board
+    position fen r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1 moves e2a6 e8g8
+*/
+
+// parse UCI "position" command
+void parse_position(char *command)
+{
+    // shift pointer to the right where next token begins [ 9 is 'position ']
+    command += 9;
+    
+    // init pointer to the current character in the command string
+    char *current_char = command;
+    
+    // parse UCI "startpos" command [ strncmp is string compare.] still not sure how the checks == 0 works, if true is 0?
+    if (strncmp(command, "startpos", 8) == 0)
+        // init chess board with start position
+        parse_fen(start_position);
+    
+    // parse UCI "fen" command 
+    else
+    {
+        // make sure "fen" command is available within command string
+        current_char = strstr(command, "fen");
+        
+        // if no "fen" command is available within command string
+        if (current_char == NULL)
+            // init chess board with start position
+            parse_fen(start_position);
+            
+        // found "fen" substring
+        else
+        {
+            // shift pointer to the right where next token begins
+            current_char += 4;
+            
+            // init chess board with position from FEN string
+            parse_fen(current_char);
+        }
+    }
+    
+    // parse moves after position
+    current_char = strstr(command, "moves");
+    
+    // moves available
+    if (current_char != NULL)
+    {
+        // shift pointer to the right where next token begins
+        current_char += 6;
+        
+        // loop over moves within a move string
+        while(*current_char)
+        {
+            // parse next move
+            int move = parse_move(current_char);
+            
+            // if no more moves
+            if (move == 0)
+                // break out of the loop
+                break;
+            
+            // make move on the chess board
+            make_move(move, all_moves);
+            
+            // move current character mointer to the end of current move
+            while (*current_char && *current_char != ' ') current_char++;
+            
+            // go to the next move
+            current_char++;
+        }
+        
+    }
+
+    // print board
+    print_board();
+}
+
+/*
+    Example UCI commands to make engine search for the best move
+    
+    // fixed depth search
+    go depth 64
+
+*/
+
+// parse UCI "go" command
+void parse_go(char *command)
+{
+    // init depth
+    int depth = -1;
+    
+    // init character pointer to the current depth argument
+    char *current_depth = NULL;
+    
+    // handle fixed depth search
+    if (current_depth = strstr(command, "depth"))
+        //convert string to integer and assign the result value to depth
+        depth = atoi(current_depth + 6);
+    
+    // different time controls placeholder
+    else
+        depth = 6;
+    
+    // search position
+    search_position(depth);
+}
+
+/*
+    GUI -> isready
+    Engine -> readyok
+    GUI -> ucinewgame
+*/
+
+// main UCI loop
+void uci_loop()
+{
+    // reset STDIN & STDOUT buffers
+    setbuf(stdin, NULL);
+    setbuf(stdout, NULL);
+    
+    // define user / GUI input buffer
+    char input[2000];
+    
+    // print engine info
+    printf("id name BitBoardChess\n");
+    printf("id author Vee\n");
+    printf("uciok\n");
+    
+    // main loop
+    while (1)
+    {
+        // reset user /GUI input
+        memset(input, 0, sizeof(input));
+        
+        // make sure output reaches the GUI
+        fflush(stdout);
+        
+        // get user / GUI input
+        if (!fgets(input, 2000, stdin))
+            // continue the loop
+            continue;
+        
+        // make sure input is available
+        if (input[0] == '\n')
+            // continue the loop
+            continue;
+        
+        // parse UCI "isready" command
+        if (strncmp(input, "isready", 7) == 0)
+        {
+            printf("readyok\n");
+            continue;
+        }
+        
+        // parse UCI "position" command
+        else if (strncmp(input, "position", 8) == 0)
+            // call parse position function
+            parse_position(input);
+        
+        // parse UCI "ucinewgame" command
+        else if (strncmp(input, "ucinewgame", 10) == 0)
+            // call parse position function
+            parse_position("position startpos");
+        
+        // parse UCI "go" command
+        else if (strncmp(input, "go", 2) == 0)
+            // call parse go function
+            parse_go(input);
+        
+        // parse UCI "quit" command
+        else if (strncmp(input, "quit", 4) == 0)
+            // quit from the chess engine program execution
+            break;
+        
+        // parse UCI "uci" command
+        else if (strncmp(input, "uci", 3) == 0)
+        {
+            // print engine info
+            printf("id name BitBoardChess\n");
+            printf("id author Vee\n");
+            printf("uciok\n");
+        }
+    }
+}
+
+/*************************************************\
+===================================================
                 Init All
 ===================================================
 \*************************************************/
@@ -2464,20 +2750,8 @@ int main() {
     // Init all
     init_all();
 
-    // parse fen
-    parse_fen(tricky_position);
-    print_board();
+    uci_loop();
 
-    // start tracking time
-    int start = get_time_ms();
-
-    // perft
-    perft_test(5);
-    getchar();
-        
-    // time taken to execute program
-    // printf("time taken to execute: %d ms\n", get_time_ms() - start);
-    // printf("nodes: %ld\n", nodes);
 
     return 0;
 
